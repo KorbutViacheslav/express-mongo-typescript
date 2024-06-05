6d5ecc993df5449b68e0b074ebd197a80c2567d3
Viacheslav Korbut <minowaman777@gmail.com>
2024-06-05 15:42:25 +0300

    some changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,8 +3,8 @@ import { mongo } from "mongoose";
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+//const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+//const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://korbutjava:korbutjava@cluster0.wtqg6as.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const SERVER_PORT = 9090 ? Number(9090) : 1337;
